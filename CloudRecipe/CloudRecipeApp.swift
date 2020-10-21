be593a64016812f28de12caa58d34834e8585a4d
P. Kurt Thorderson <pkthor@gmail.com>
2020-10-21 15:35:11 -0600

    Initial Commit

---
+++ CloudRecipe/CloudRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudRecipeApp.swift
+//  CloudRecipe
+//
+//  Created by P. Kurt Thorderson on 10/21/20.
+//
+
+import SwiftUI
+
+@main
+struct CloudRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
