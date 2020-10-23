c06ced37ad3e3ef0c9d3507c01f54c268dccbf58
P. Kurt Thorderson <pkthor@gmail.com>
2020-10-22 21:55:12 -0600

    Commit from new computer

---
+++ CloudRecipe/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  CloudRecipe
+//
+//  Created by P. Kurt Thorderson on 10/22/20.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, Dude!")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
